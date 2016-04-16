cc88d3633b06aca7b73d7e2891a5569d1fa0aa50
Zhao Weng <w2280518650@gmail.com>
2016-04-16 11:32:14 -0500

    three pages

--- app/src/main/java/com/example/zweng4/menubox/MainActivity.java
+++ app/src/main/java/com/example/zweng4/menubox/MainActivity.java
@@ -5,13 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login_btn;
+    private Button signin_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signin_btn = (Button) findViewById(R.id.signin_button);
+        login_btn = (Button) findViewById(R.id.login_button);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
+    signin_btn.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            if (operand1.getText().length() > 0 && operand2.getText().length() > 0) {
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+                double theResult = oper1 + oper2;
+                result.setText(Double.toString(theResult));
+            } else
+                Toast.makeText(MainActivity.this, "Please enter numbers in both operand", Toast.LENGTH_LONG).show();
+        }
+    });
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
