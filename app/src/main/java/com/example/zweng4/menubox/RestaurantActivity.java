e5bcefccb31c9c2a51b7c135f35bff5204452d7f
Zhao Weng <w2280518650@gmail.com>
2016-04-21 22:00:44 -0500

    more features

--- app/src/main/java/com/example/zweng4/menubox/RestaurantActivity.java
+++ app/src/main/java/com/example/zweng4/menubox/RestaurantActivity.java
@@ -1,11 +1,13 @@
 package com.example.zweng4.menubox;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageButton;
 
@@ -36,7 +38,16 @@ public class RestaurantActivity extends AppCompatActivity {
         //get the gridView
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Send intent to SingleViewActivity
+                Intent i = new Intent(getApplicationContext(), SingleViewActivity.class);
 
+                // Pass image index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
 
 
     }
