68c630f1a250d3beccf910a489a32539464b0c74
Zhao Weng <w2280518650@gmail.com>
2016-04-26 16:38:37 -0500

    onclick

--- app/src/main/java/com/example/zweng4/menubox/FavouriteActivity.java
+++ app/src/main/java/com/example/zweng4/menubox/FavouriteActivity.java
@@ -1,19 +1,26 @@
 package com.example.zweng4.menubox;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class FavouriteActivity extends AppCompatActivity {
-    String[] mobileArray = {" "," "," "," "," "," "," "," "};
+    String[] mobileArray = {" ", " ", " ", " ", " ", " ", " ", " "};
     private EditText fav_txt;
     private ImageButton search_btn;
     private SQLiteDatabase sqLiteDatabase;
@@ -27,13 +34,19 @@ public class FavouriteActivity extends AppCompatActivity {
     private int max;
     private String name;
     private String location;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
         adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
-        fav_txt = (EditText)findViewById(R.id.fav_txt);
-        search_btn = (ImageButton)findViewById(R.id.search_btn);
+        fav_txt = (EditText) findViewById(R.id.fav_txt);
+        search_btn = (ImageButton) findViewById(R.id.search_btn);
         sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test-1.db", MODE_PRIVATE, null);
         //factory optional to instantiate a cursor when query is called.
 
@@ -51,14 +64,13 @@ public class FavouriteActivity extends AppCompatActivity {
 
         max = 0;
         sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test-1.db", MODE_PRIVATE, null);
-        query = sqLiteDatabase.rawQuery("SELECT * FROM restaurants" , null);
+        query = sqLiteDatabase.rawQuery("SELECT * FROM restaurants", null);
         if (query.moveToFirst()) {
-            do  {//cursor is empty if there is no data, else move to the first data
-                max ++;
+            do {//cursor is empty if there is no data, else move to the first data
+                max++;
 
             } while (query.moveToNext());
-        }
-        else Toast.makeText(getBaseContext(), "No restaurant match", Toast.LENGTH_LONG).show();
+        } else Toast.makeText(getBaseContext(), "No restaurant match", Toast.LENGTH_LONG).show();
         sqLiteDatabase.close(); //count number of selected tuples.
 
         sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test-1.db", MODE_PRIVATE, null);
@@ -85,29 +97,26 @@ public class FavouriteActivity extends AppCompatActivity {
         sqLiteDatabase.close();
 
 
-
         listView = (ListView) findViewById(R.id.xmlListView);
         listView.setAdapter(adapter);
-
-
 
 
         search_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                for(i = 0; i <8;i++)
+                for (i = 0; i < 8; i++)
                     mobileArray[i] = " ";
                 fav = fav_txt.getText().toString();
                 max = 0;
                 sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test-1.db", MODE_PRIVATE, null);
-                query = sqLiteDatabase.rawQuery("SELECT * FROM restaurants WHERE name = '" + fav + "'" , null);
+                query = sqLiteDatabase.rawQuery("SELECT * FROM restaurants WHERE name = '" + fav + "'", null);
                 if (query.moveToFirst()) {
-                    do  {//cursor is empty if there is no data, else move to the first data
-                        max ++;
+                    do {//cursor is empty if there is no data, else move to the first data
+                        max++;
 
                     } while (query.moveToNext());
-                }
-                else Toast.makeText(getBaseContext(), "No restaurant match", Toast.LENGTH_LONG).show();
+                } else
+                    Toast.makeText(getBaseContext(), "No restaurant match", Toast.LENGTH_LONG).show();
                 sqLiteDatabase.close(); //count number of selected tuples.
 
                 sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test-1.db", MODE_PRIVATE, null);
@@ -134,9 +143,9 @@ public class FavouriteActivity extends AppCompatActivity {
                 sqLiteDatabase.close();
 
 
-
                 listView = (ListView) findViewById(R.id.xmlListView);
                 listView.setAdapter(adapter);
+
 
             }
         });
@@ -144,6 +153,7 @@ public class FavouriteActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -153,6 +163,72 @@ public class FavouriteActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        //Add intent for Phone
+                        startActivity(new Intent("RestaurantActivity"));
+                        break;
+                    case 1:
+                        //Add intent for Email
+                        break;
+                    case 2:
+                        //Add intent for Website
+                        break;
+                    case 3:
+                        //Add intent for Opening Time
+                        break;
+
+                }
+            }
+        });
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Favourite Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zweng4.menubox/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Favourite Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zweng4.menubox/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
